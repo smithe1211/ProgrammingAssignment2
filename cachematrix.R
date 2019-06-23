446c4500a71dd6262cfefa2f67381a86f4625d52
smithe1211 <smithe1211@gmail.com>
2019-06-23 14:11:36 -0400

    This is my Week 3 programming assignement for R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will cache the mean values into a matrix
+## and store the inverse of the matrix
 
-## Write a short comment describing this function
+## This function will cache a special matrix of mean values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) i <<- mean
+        getmean <- function() i
+        list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+
+
+
+## this function checks if the inverse has already been calculated and returns that
+## or calculates the inverse and returns that value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getmean()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setmean(i)
+        i
 }
